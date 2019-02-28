eb3f34d0c67f7408b0cc6861b3fbf042c059a4bc
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-02-28 10:56:19 -0500

    Refactor detail view

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls' #When you have more applications you specify the name
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results', views.results, name='results'),
     # ex: /polls/5/vote/
