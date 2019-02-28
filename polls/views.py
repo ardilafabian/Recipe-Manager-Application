dd51373de7b4341177141f37fb22780b150784d3
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-02-28 10:08:16 -0500

    I plementing iews and emplates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+    latest_quesion_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_quesion_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of uestion %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
