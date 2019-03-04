4f3a232b41ed1c142ca8dc01fe96e7e513fb7c87
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-03-04 16:06:04 -0500

    Change models

--- recipeManager/models.py
+++ recipeManager/models.py
@@ -29,6 +29,7 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     ingredients = models.ManyToManyField(Ingredient, through='Recipe_Ingredients')
 
     def __str__(self):
