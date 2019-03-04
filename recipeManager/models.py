be302ecf655ff01d26aa71b63fb26d54d37a8276
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-03-04 12:40:02 -0500

    Create recipeManager models

--- recipeManager/models.py
+++ recipeManager/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    cost = models.FloatField()
+    amount = models.FloatField()
+    #-Unit Choices----
+    GRAMS='g'
+    KILOGRAMS='Kg'
+    CENTILITER='cl'
+    LITER='l'
+    UNIT_CHOICES = (
+        (GRAMS, 'Grams'),
+        (KILOGRAMS, 'Kilograms'),
+        (CENTILITER, 'Centiliter'),
+        (LITER, 'Liter')
+    )
+    #------------------
+    unit = models.CharField(
+        max_length=2,
+        choices=UNIT_CHOICES,
+        default=GRAMS,
+    )
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(Ingredient, through='Recipe_Ingredients')
+
+class Recipe_Ingredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
--- recipeSite/settings.py
+++ recipeSite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'recipeManager.apps.RecipemanagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
