5acd13a3c04ee1aa883478fb6f783883c429470b
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2019-03-07 15:41:11 -0500

    Improve view

--- recipeManager/static/recipeManager/addRecipe.js
+++ recipeManager/static/recipeManager/addRecipe.js
@@ -12,7 +12,6 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   console.log(data)
 
-  $("#data").hide();
   var emptyRow = document.getElementById("empty");
   if(emptyRow){
     emptyRow.parentNode.removeChild(emptyRow);
@@ -21,8 +20,16 @@ function drop(ev) {
   var tr = document.createElement('tr');
   var td2 = tr.appendChild(document.createElement('td'));
   td2.innerHTML = $("#name"+ data)[0].innerHTML;
+
+  var input = document.createElement('input');
+  input.type = "number";
+  input.step = "0.01";
+  input.name = "quantity";
+  input.required = true;
+  input.className = "form-control text-dark";
   var td1 = tr.appendChild(document.createElement('td'));
-  td1.innerHTML = 1.0;
+  td1.appendChild(input);
+
   var td3 = tr.appendChild(document.createElement('td'));
   td3.innerHTML = $("#unit"+ data)[0].innerHTML;
 
